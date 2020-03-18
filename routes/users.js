c1418f5cbcf8c8dadbd528cbd11d2774ee554d34
nappingCommodore <niks.sbg@gmail.com>
2020-03-18 20:23:48 +0530

    login route added

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,38 @@ router.get("/current", auth, async (req, res) => {
   res.send(user);
 });
 
+router.post("/login", async (req, res) => {
+  console.log(req.body);
+  let user;
+  if(req.body.email)
+    user = await User.findOne({ email: req.body.email});
+  else if(req.body.name)
+    user = await User.findOne({ name: req.body.name});
+  console.log("USER IS => ", user);
+  if(!user) {
+    res.status(401).send({"error": "username or Email is incorrect."})
+  } else if (user) {
+    let isValidPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!isValidPassword) {
+      res.status(401).send({"error": "password is incorrect."});
+    } else {
+      const token = user.generateAuthToken();
+      res.header("x-auth-token", token).send({
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      });
+    }
+  }
+  // user = new User({
+  //   name: req.body.name,
+  //   password: req.body.password,
+  //   email: req.body.email,
+  //   role: req.body.role,
+  // });
+  
+});
+
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
